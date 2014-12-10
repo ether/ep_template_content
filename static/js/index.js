dd626a4449389a7f0051b73276c127d6ae16bc92
John McLear <john@mclear.co.uk>
2014-12-10 16:55:48 +0000

    working highlight and pre-population

--- static/js/index.js
+++ static/js/index.js
@@ -2,16 +2,41 @@ var $ = require('ep_etherpad-lite/static/js/rjquery').$;
 
 // Bind the event handler to the toolbar buttons
 exports.postAceInit = function(hook, context){
+
+  // Register UI elements and listeners
   padeditbar.registerDropdownCommand("template_content");
   var hs = $('#template_content-selection');
   hs.on('change', function(){
     var value = $(this).val();
     if(value >= 0){
-      showInput(value);
+      // set the current highlighted text to a value
+      template_content.getHighlighted(context);
+      template_content.showInput(value);
     }
   })
 
   $('body').on("click", "#template_content-save", function(){
+    // get the input contents
+    template_content.getInputContents(function(content){
+      // write the input contents
+      template_content.placeContentInPad(content);
+    });
+  });
+};
+
+var template_content = {
+
+  getHighlighted: function(context){
+    self.highLightedString = "";
+    context.ace.callWithAce(function(ace){
+      var rep = ace.ace_getRep();
+      var line = rep.lines.atIndex(rep.selStart[0]);
+      self.highLightedString = line.text.substring(rep.selStart[1], rep.selEnd[1]);
+      console.warn(self.highLightedString);
+    }, 'template_content_highLight', true);
+  },
+
+  getInputContents: function(cb){
     // For each input type in the dropdown
     var inputs = $("#template_content > div > textarea:visible, #template_content > div > input:visible, #template_content > div > checkbox:visible");
     var content = "";
@@ -22,22 +47,34 @@ exports.postAceInit = function(hook, context){
       content += label + ": "+input + "\n";
       console.warn(label, input);
     });
-    placeContentInPad(content);
-  });
-};
+    cb(content);
+  },
 
-function showInput(value){
-  $("#template_content > div").hide(); // hide all others
-  $("#template_content_"+value).show(); // show this input
-  padeditbar.toggleDropDown("template_content");
-}
+  showInput: function(value){
+    var padeditor = require('ep_etherpad-lite/static/js/pad_editor').padeditor;
+    console.warn("ace", padeditor.ace);
 
-function placeContentInPad(content){
-  var padeditor = require('ep_etherpad-lite/static/js/pad_editor').padeditor;
-  // Puts the completed form data in the pad.
-  padeditor.ace.replaceRange(undefined, undefined, content);
-  // Put the caret back into the pad
-  padeditor.ace.focus();
+    $("#template_content > div").hide(); // hide all others
+    $("#template_content_"+value).show(); // show this input
+    padeditbar.toggleDropDown("template_content");
+
+    // If highLigtedString is available pre-populate first input..
+    if(self.highLightedString){
+      // Set the first input as the value
+      var inputs = $("#template_content > div > textarea:visible, #template_content > div > input:visible, #template_content > div > checkbox:visible");
+      $(inputs[0]).val(self.highLightedString);
+    }
+  },
+
+  placeContentInPad: function(content){
+    var padeditor = require('ep_etherpad-lite/static/js/pad_editor').padeditor;
+    // Puts the completed form data in the pad.
+    padeditor.ace.replaceRange(undefined, undefined, content);
+    // Put the caret back into the pad
+    padeditor.ace.focus();
+  },
+
+  highLightedString: ""
 }
 
 
